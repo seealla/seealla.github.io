c8348a40b3bab3912c5273e7ab0d065cf47cbba3
Lau Ching Jun <lauchingjun@hotmail.com>
2015-01-01 01:29:56 +0800

    Initial commit

---
+++ image_list.js
@@ -0,0 +1,3 @@
+seealla_photo('images/_MG_0124.JPG', 'thumbnails/_MG_0124.JPG', false);
+seealla_photo('images/IMG_4099.jpg', 'thumbnails/IMG_4099.jpg', false);
+seealla_photo('images/IMG_4903.jpg', 'thumbnails/IMG_4903.jpg', false);
