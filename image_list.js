014e83d667dabf8ac2f376ff58de00ec3d1d7144
Lau Ching Jun <lauchingjun@hotmail.com>
2015-01-26 14:12:52 +0800

    New photos on top

--- image_list.js
+++ image_list.js
@@ -1,6 +1,6 @@
-seealla_photo('images/_MG_0124.JPG', 'thumbnails/_MG_0124.JPG');
-seealla_photo('images/IMG_4099.jpg', 'thumbnails/IMG_4099.jpg');
-seealla_photo('images/IMG_4903.jpg', 'thumbnails/IMG_4903.jpg');
 seealla_photo('images/IMG_0329.jpg', 'thumbnails/IMG_0329.jpg');
 seealla_photo('images/20150122114857__MG_3046-1.jpg', 'thumbnails/20150122114857__MG_3046-1.jpg');
 seealla_photo('images/_MG_9522.jpg', 'thumbnails/_MG_9522.jpg');
+seealla_photo('images/_MG_0124.JPG', 'thumbnails/_MG_0124.JPG');
+seealla_photo('images/IMG_4099.jpg', 'thumbnails/IMG_4099.jpg');
+seealla_photo('images/IMG_4903.jpg', 'thumbnails/IMG_4903.jpg');
